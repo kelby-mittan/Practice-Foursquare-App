6359564f63294e2ee7cd3eee0318e3721015ea57
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-22 20:43:44 -0500

    setup ui for map view controller

--- Practice-Foursquare-App/Views/TheMapView.swift
+++ Practice-Foursquare-App/Views/TheMapView.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 class TheMapView: UIView {
 
     public lazy var locationSearchBar: UISearchTextField = {
         let sb = UISearchTextField()
         sb.placeholder = "search city"
-        
+        sb.backgroundColor = .systemBackground
         return sb
+    }()
+    
+    public lazy var mapView: MKMapView = {
+        let mv = MKMapView()
+        return mv
     }()
     
     override init(frame: CGRect) {
@@ -28,6 +34,8 @@ class TheMapView: UIView {
     }
     
     private func commonInit() {
+        
+        setupMapViewConstraints()
         setupLocationSearchBarConstraints()
     }
     
@@ -37,8 +45,21 @@ class TheMapView: UIView {
         
         NSLayoutConstraint.activate([
             locationSearchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
-            locationSearchBar.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-            locationSearchBar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
+            locationSearchBar.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            locationSearchBar.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor, multiplier: 0.8)
+//            locationSearchBar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
+        ])
+    }
+    
+    private func setupMapViewConstraints() {
+        addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 
